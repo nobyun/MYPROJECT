40b5ae37692391a9f502aaa075709923a2f523f3
nobyun <148747445+nobyun@users.noreply.github.com>
2025-03-18 15:21:58 +0800

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+from django.http import HttpResponse
+def sayhello(request):
+	return HttpResponse("Hello 劉老師!")
+def hello2(request, username):
+	return HttpResponse("Hello 2 " + username)
+def hello3(request, username):
+	now=datetime.now()
+	return render(request, "hello3.html",locals())
